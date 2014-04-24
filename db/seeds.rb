f2feb237b1e645e43ce9ecc043854c847943754f
Caleb Tutty <caleb@carnivallabs.com>
2014-04-24 14:49:25 +1200

    Make seed data idempotent

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,66 +13,83 @@ require 'csv'
 # if it does not then download https://s3-ap-southeast-2.amazonaws.com/censusnz/1_meshblock_geometries.csv
 #
 
-puts "Creating Area Units"
+if AreaUnit.count == 0
+  puts "Creating Area Units"
 
-CSV.foreach(Rails.root.join('db', 'seeds', 'meshblocks', '2_area_units.csv'), headers: true) do |attrs|
-  AreaUnit.create(id: attrs["id"].to_i, name: attrs["name"])
+  CSV.foreach(Rails.root.join('db', 'seeds', 'meshblocks', '2_area_units.csv'), headers: true) do |attrs|
+    AreaUnit.create(id: attrs["id"].to_i, name: attrs["name"])
+  end
 end
 
+if UrbanArea.count == 0
+  puts "Creating Urban Areas"
 
-puts "Creating Urban Areas"
-
-CSV.foreach(Rails.root.join('db', 'seeds', 'meshblocks', '3_urban_areas.csv'), headers: true) do |attrs|
-  UrbanArea.create(id: attrs["id"].to_i, name: attrs["name"])
+  CSV.foreach(Rails.root.join('db', 'seeds', 'meshblocks', '3_urban_areas.csv'), headers: true) do |attrs|
+    UrbanArea.create(id: attrs["id"].to_i, name: attrs["name"])
+  end
 end
 
-puts "Creating Territorial Authorities"
+if TerritorialAuthority.count == 0
+  puts "Creating Territorial Authorities"
 
-CSV.foreach(Rails.root.join('db', 'seeds', 'meshblocks', '4_territorial_authorities.csv'), headers: true) do |attrs|
-  TerritorialAuthority.create(id: attrs["id"].to_i, name: attrs["name"])
+  CSV.foreach(Rails.root.join('db', 'seeds', 'meshblocks', '4_territorial_authorities.csv'), headers: true) do |attrs|
+    TerritorialAuthority.create(id: attrs["id"].to_i, name: attrs["name"])
+  end
 end
 
+if Ward.count == 0
+  puts "Creating Wards"
 
-puts "Creating Wards"
-
-CSV.foreach(Rails.root.join('db', 'seeds', 'meshblocks', '5_wards.csv'), headers: true) do |attrs|
-  Ward.create(id: attrs["id"].to_i, name: attrs["name"])
+  CSV.foreach(Rails.root.join('db', 'seeds', 'meshblocks', '5_wards.csv'), headers: true) do |attrs|
+    Ward.create(id: attrs["id"].to_i, name: attrs["name"])
+  end
 end
 
-puts "Creating Community Boards"
+if CommunityBoard.count == 0
+  puts "Creating Community Boards"
 
-CSV.foreach(Rails.root.join('db', 'seeds', 'meshblocks', '6_community_boards.csv'), headers: true) do |attrs|
-  CommunityBoard.create(id: attrs["id"].to_i, name: attrs["name"])
+  CSV.foreach(Rails.root.join('db', 'seeds', 'meshblocks', '6_community_boards.csv'), headers: true) do |attrs|
+    CommunityBoard.create(id: attrs["id"].to_i, name: attrs["name"])
+  end
 end
 
-puts "Creating Territorial Authority Subdivisions"
+if TerritorialAuthoritySubdivision.count == 0
+  puts "Creating Territorial Authority Subdivisions"
 
-CSV.foreach(Rails.root.join('db', 'seeds', 'meshblocks', '7_territorial_authority_subdivisions.csv'), headers: true) do |attrs|
-  TerritorialAuthoritySubdivision.create(id: attrs["id"].to_i, name: attrs["name"])
+  CSV.foreach(Rails.root.join('db', 'seeds', 'meshblocks', '7_territorial_authority_subdivisions.csv'), headers: true) do |attrs|
+    TerritorialAuthoritySubdivision.create(id: attrs["id"].to_i, name: attrs["name"])
+  end
 end
 
-puts "Creating Regional Councils"
+if RegionalCouncil.count == 0
+  puts "Creating Regional Councils"
 
-CSV.foreach(Rails.root.join('db', 'seeds', 'meshblocks', '8_regional_councils.csv'), headers: true) do |attrs|
-  RegionalCouncil.create(id: attrs["id"].to_i, name: attrs["name"])
+  CSV.foreach(Rails.root.join('db', 'seeds', 'meshblocks', '8_regional_councils.csv'), headers: true) do |attrs|
+    RegionalCouncil.create(id: attrs["id"].to_i, name: attrs["name"])
+  end
 end
 
-puts "Creating Regional Council Constituencies"
+if RegionalCouncilConstituency.count == 0
+  puts "Creating Regional Council Constituencies"
 
-CSV.foreach(Rails.root.join('db', 'seeds', 'meshblocks', '9_regional_council_constituencies.csv'), headers: true) do |attrs|
-  RegionalCouncilConstituency.create(id: attrs["id"].to_i, name: attrs["name"])
+  CSV.foreach(Rails.root.join('db', 'seeds', 'meshblocks', '9_regional_council_constituencies.csv'), headers: true) do |attrs|
+    RegionalCouncilConstituency.create(id: attrs["id"].to_i, name: attrs["name"])
+  end
 end
 
-puts "Creating Regional Council Maori Constituencies"
+if RegionalCouncilMaoriConstituency.count == 0
+  puts "Creating Regional Council Maori Constituencies"
 
-CSV.foreach(Rails.root.join('db', 'seeds', 'meshblocks', '10_regional_council_maori_constituencies.csv'), headers: true) do |attrs|
-  RegionalCouncilMaoriConstituency.create(id: attrs["id"].to_i, name: attrs["name"])
+  CSV.foreach(Rails.root.join('db', 'seeds', 'meshblocks', '10_regional_council_maori_constituencies.csv'), headers: true) do |attrs|
+    RegionalCouncilMaoriConstituency.create(id: attrs["id"].to_i, name: attrs["name"])
+  end
 end
 
-puts "Creating Land Types"
+if LandType.count == 0
+  puts "Creating Land Types"
 
-CSV.foreach(Rails.root.join('db', 'seeds', 'meshblocks', '11_land_types.csv'), headers: true) do |attrs|
-  LandType.create(id: attrs["id"].to_i, name: attrs["name"])
+  CSV.foreach(Rails.root.join('db', 'seeds', 'meshblocks', '11_land_types.csv'), headers: true) do |attrs|
+    LandType.create(id: attrs["id"].to_i, name: attrs["name"])
+  end
 end
-
 
