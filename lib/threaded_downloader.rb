a9bc24f9e3cbb2e612775e951bde2f937b3e7696
Caleb Tutty <caleb@carnivallabs.com>
2014-04-24 18:25:16 +1200

    Made threaded downloader write to file

--- lib/threaded_downloader.rb
+++ lib/threaded_downloader.rb
@@ -2,21 +2,24 @@ require 'net/http'
 require 'uri'
 
 module ThreadedDownloader
-  def self.download(url)
+  def self.download(url, options)
+    options.assert_valid_keys(:to)
+    file_handle = File.open(options[:to], 'w')
+
     Thread.new do
       thread = Thread.current
-      body = thread[:body] = []
 
       url = URI.parse url
       Net::HTTP.new(url.host, url.port).request_get(url.path) do |response|
         length = thread[:length] = response['Content-Length'].to_i
 
         response.read_body do |fragment|
-          body << fragment
+          file_handle.write fragment
           thread[:done] = (thread[:done] || 0) + fragment.length
           thread[:progress] = thread[:done].quo(length) * 100
         end
       end
     end
+    file_handle.close
   end
 end
