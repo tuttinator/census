1f45574d68a6b59df3d99295fed9638102bbcfce
Caleb Tutty <caleb@prettymint.co.nz>
2014-07-31 23:30:00 +1200

    Added NzscOcc guard

--- db/seeds.rb
+++ db/seeds.rb
@@ -570,22 +570,23 @@ if NightPopulation.count == 0
   end
 end
 
-
-# NzscOccupations
-puts 'Importing NzscOccupation...'
-CSV.foreach(Rails.root.join('db', 'seeds', 'census_data', 'nzsc_occupations.csv'), headers: true) do |attrs|
-  NzscOccupation.create(meshblock_id: attrs['meshblock_id'],
-                        census_year_id: attrs['census_year_id'],
-                        agriculture_and_fishery_workers: attrs['agriculture_and_fishery_workers'],
-                        clerks: attrs['clerks'],
-                        elementary_occupations: attrs['elementary_occupations'],
-                        legislators_admins_and_managers: attrs['legislators_admins_and_managers'],
-                        plant_and_machine_operators: attrs['plant_and_machine_operators'],
-                        professionals: attrs['professionals'],
-                        service_and_sales_workers: attrs['service_and_sales_workers'],
-                        technicians_and_associate_professionals: attrs['technicians_and_associate_professionals'],
-                        total: attrs['total'],
-                        trades_workers: attrs['trades_workers'])
+if NzscOccupation.count == 0
+  # NzscOccupations
+  puts 'Importing NzscOccupation...'
+  CSV.foreach(Rails.root.join('db', 'seeds', 'census_data', 'nzsc_occupations.csv'), headers: true) do |attrs|
+    NzscOccupation.create(meshblock_id: attrs['meshblock_id'],
+                          census_year_id: attrs['census_year_id'],
+                          agriculture_and_fishery_workers: attrs['agriculture_and_fishery_workers'],
+                          clerks: attrs['clerks'],
+                          elementary_occupations: attrs['elementary_occupations'],
+                          legislators_admins_and_managers: attrs['legislators_admins_and_managers'],
+                          plant_and_machine_operators: attrs['plant_and_machine_operators'],
+                          professionals: attrs['professionals'],
+                          service_and_sales_workers: attrs['service_and_sales_workers'],
+                          technicians_and_associate_professionals: attrs['technicians_and_associate_professionals'],
+                          total: attrs['total'],
+                          trades_workers: attrs['trades_workers'])
+  end
 end
 
 # NzscWorkplaceOccupations
